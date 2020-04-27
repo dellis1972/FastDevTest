5101bbfb5495daf97d8209478e1ae23f7a8472be
Dean Ellis <dellis1972@googlemail.com>
2020-04-27 16:28:26 +0100

    inital commit

---
+++ foo.UITests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace foo.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = ConfigureApp.Android.StartApp();
+		}
+
+		[Test]
+		public void AppDidLaunch()
+		{
+			app.WaitForElement(c => c.Id("fab"));
+			app.Tap(c => c.Id("fab"));
+			Assert.AreNotEqual("ERROR!", app.Query(c => c.Id("textView1"))[0].Text);
+			app.Screenshot("App launched");
+		}
+	}
+}
